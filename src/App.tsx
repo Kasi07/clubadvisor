d72e488cb3ca10fe7907caf450ec7a8175f273b3
Kasimir Weilandt <Kasimir@Weilandt.it>
2023-06-01 08:32:21 +0200

    forgot to stage lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import StartPage from "./components/StartPage";
 import SearchPage from "./components/SearchPage";
 import ClubPage from "./components/ClubPage";
@@ -6,13 +6,15 @@ import EventPage from "./components/EventPage";
 
 function App() {
   return (
-    <StartPage />
-    // <Router>
-    //   <Route path="/" Component={StartPage} />
-    //   <Route path="/search" Component={SearchPage} />
-    //   <Route path="/club/:clubid(\d*)" Component={ClubPage} />
-    //   <Route path="/event/:eventid(\d*)" Component={EventPage} />
-    // </Router>
+    // <StartPage />
+    <Router>
+      <Routes>
+        <Route path="/" Component={StartPage} />
+        <Route path="/search" Component={SearchPage} />
+        <Route path="/club/:clubid(\d*)" Component={ClubPage} />
+        <Route path="/event/:eventid(\d*)" Component={EventPage} />
+      </Routes>
+    </Router>
   );
 }
 
